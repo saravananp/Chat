a7e45d1ffd3de53610f56563ec2da939717a3f97
Saravanan <saravanan.p@shriramits.com>
2012-04-04 21:12:07 +0530

    chatting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //= require_tree .
 
 $(function() {
-  var faye = new Faye.Client('http://localhost:9292/faye');
+  var faye = new Faye.Client('http://saran.heroku.com/faye');
   faye.subscribe("/messages/new", function(data) {
     eval(data);
   });
